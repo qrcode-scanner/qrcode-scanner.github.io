f28d19db31365d1ab2224db567306911dd6fc4d4
Rizwankmc <rizwanssv@gmail.com>
2022-06-14 10:53:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import QrCodeScanner from "./qrCodeScanner";
+
+function App() {
+  return (
+    <div className="App">
+      <QrCodeScanner />
+    </div>
+  );
+}
+
+export default App;
